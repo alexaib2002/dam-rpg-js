8f8527dd570b890d10bfecb29ce18b0e48bd747e
alexaib <alexaib@outlook.es>
2022-05-10 18:54:02 +0200

    basic fight ui implemented

--- js/entities/buttons.js
+++ js/entities/buttons.js
@@ -2,25 +2,68 @@ import * as me from 'https://esm.run/melonjs';
 import gameController from '../../index.js';
 
 export class ButtonUI extends me.GUI_Object {
-    constructor(x, y, label, color) {
+
+    constructor(x, y, color, label) {
         super(x, y, {
             image: gameController.texture,
-            region: color + "_button06"
+            region : color + "_button04"
         });
-        this.label = label;
+
+        // offset of the two used images in the texture
+        this.unclicked_region = gameController.texture.getRegion(color + "_button04");
+        this.clicked_region = gameController.texture.getRegion(color + "_button05");
+
         this.anchorPoint.set(0, 0);
+        this.setOpacity(0.5);
 
         this.font = new me.Text(0, 0 ,{
             font: "kenpixel",
-            size: 24,
-            fillStyle: "white",
+            size: 12,
+            fillStyle: "black",
             textAlign: "center",
             textBaseline: "middle",
             offScreenCanvas: (me.video.renderer.WebGLVersion >= 1)
         });
 
+        this.label = label;
+
+        // only the parent container is a floating object
         this.floating = false;
-        this.pos.z = 4;
+    }
+
+    /**
+     * function called when the pointer is over the object
+     */
+    onOver(/* event */) {
+        this.setOpacity(1.0);
+    }
+
+    /**
+     * function called when the pointer is leaving the object area
+     */
+    onOut(/* event */) {
+        this.setOpacity(0.5);
+    }
+
+    /**
+     * function called when the object is clicked on
+     */
+    onClick(event) {
+        console.log("clicked on " + this.label);
+        this.translate(0, this.height - this.clicked_region.height);
+        this.setRegion(this.clicked_region);
+        // don't propagate the event
+        return false;
+    }
+
+    /**
+     * function called when the pointer button is released
+     */
+    onRelease(/* event */) {
+        this.setRegion(this.unclicked_region);
+        this.translate(0, -(this.height - this.clicked_region.height));
+        // don't propagate the event
+        return false;
     }
 
     draw(renderer) {
@@ -30,7 +73,5 @@ export class ButtonUI extends me.GUI_Object {
             this.pos.x + this.width / 2,
             this.pos.y + this.height / 2
         );
-        console.log(this.pos);
     }
 };
-
--- js/screens/battle.js
+++ js/screens/battle.js
@@ -1,24 +1,23 @@
 import * as me from 'https://esm.run/melonjs';
 import BattleUIContainer from '../ui/BattleUIContainer.js';
-import {ButtonUI} from '../entities/buttons.js';
+import { ButtonUI } from '../entities/buttons.js';
 
 class BattleScreen extends me.Stage {
 
     onResetEvent() {
         // clear bg
-        me.game.world.addChild(new me.ColorLayer("background", "#202020"));
+        me.game.world.addChild(new me.ColorLayer("background", "#202020"), 0);
 
         // init ui elements
         var panel = new BattleUIContainer(
-            me.game.viewport.width, me.game.viewport.height, 0 ,0
+            10, 470, 780, 120 // FIXME hardcoded
         );
 
-        var buttonList = [
-            new ButtonUI(-100,-100, "TestingButton", "blue")
-        ];
-        for (var button in buttonList) {
-            panel.addChild(buttonList[button]);
-        }
+        // FIXME more hardcode
+        panel.addChild(new ButtonUI(20, 40, "blue", "Attack"));
+        panel.addChild(new ButtonUI(230, 40, "yellow", "Defend"));
+        panel.addChild(new ButtonUI(440, 40, "green", "Recover"));
+        panel.addChild(new ButtonUI(650, 40, "red", "Esc"));
 
         me.game.world.addChild(panel, 1);
     }
