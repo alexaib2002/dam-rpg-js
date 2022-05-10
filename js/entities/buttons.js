5f5ccf123087cf5e5b70c56bedfae421913cd81f
alexaib <alexaib@outlook.es>
2022-05-10 16:52:04 +0200

    AtlasMap texture and font implemented for ButtonUI

--- js/entities/buttons.js
+++ js/entities/buttons.js
@@ -1,17 +1,37 @@
 import * as me from 'https://esm.run/melonjs';
+import resourceHolder from '../../index.js';
 
 
 export class ButtonUI extends me.GUI_Object {
-    /**
-     * constructor
-     */
-    constructor(x, y) {
+    constructor(x, y, label, color) {
         super(x, y, {
-            image: me.loader.getImage("defButton"),
+            image: resourceHolder.texture,
+            region: color + "_button06"
         });
+        this.label = label;
         this.anchorPoint.set(0, 0);
+
+        this.font = new me.Text(0, 0 ,{
+            font: "kenpixel",
+            size: 24,
+            fillStyle: "white",
+            textAlign: "center",
+            textBaseline: "middle",
+            offScreenCanvas: (me.video.renderer.WebGLVersion >= 1)
+        });
+
         this.floating = false;
         this.pos.z = 4;
+    }
+
+    draw(renderer) {
+        super.draw(renderer);
+        this.font.draw(renderer,
+            this.label,
+            this.pos.x + this.width / 2,
+            this.pos.y + this.height / 2
+        );
+        console.log(this.pos);
     }
 };
 
