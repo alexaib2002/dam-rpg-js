59009362baa186ecad18b90fb02455adce370839
JunZhou <zhoujun97099@gmail.com>
2022-05-14 22:57:09 +0200

    added defend to the constructor

--- js/entities/battleentity.js
+++ js/entities/battleentity.js
@@ -1,9 +1,10 @@
 export class BattleEntity {
 
-    constructor(name, health, attacks) {
+    constructor(name, health, attacks, defend) {
         this.name = name;
         this.health = health;
         this.attacks = attacks;
+        this.defend = defend;
     }
 
     
