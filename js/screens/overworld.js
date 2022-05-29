6d8509c681288a7c1464996163781cfdd969feca
alexaib <alexaib@outlook.es>
2022-05-29 18:52:39 +0200

    added resources for house interiors, changed default spawn location

--- js/screens/overworld.js
+++ js/screens/overworld.js
@@ -3,7 +3,7 @@ import * as me from "https://esm.run/melonjs";
 export default class OverworldScreen extends me.Stage {
 
     onResetEvent() {
-        me.level.load("room01", {});
+        me.level.load("room11_2", {});
 
 
         // override engine gravity calculations
