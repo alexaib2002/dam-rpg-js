6d6061c16b8a5d318ead65b9f4f812f19c919913
JunZhou <zhoujun97099@gmail.com>
2022-05-29 13:14:19 +0200

    added palceholder validation

--- www/form.js
+++ www/form.js
@@ -1,22 +1,42 @@
-// validate form
+// validacion hecha con copilot, placeholder
 function validateForm() {
-    var x = document.forms["form.html"]["nickname"].value;
+    
+    var x = document.forms["form"]["nickname"].value;
     if (x == "") {
         alert("Name must be filled out");
         return false;
     }
-
-    var x = document.forms["form.html"]["email"].value;
+    
+    var x = document.forms["form"]["email"].value;
     if (x == "") {
         alert("Email must be filled out");
         return false;
     }
-    var x = document.forms["form.html"]["password"].value;
+    
+    var x = document.forms["form"]["password"].value;
     if (x == "") {
         alert("Password must be filled out");
         return false;
     }
-    
+
+    // check for only 1 checkbox checked and no more than 1
+    var checkboxes = document.getElementsByName('playertype');
+    var checked = false;
+    for (var i = 0; i < checkboxes.length; i++) {
+        if (checkboxes[i].checked) {
+            if (checked) {
+                alert("You can only select player type");
+                return false;
+            }
+            checked = true;
+        }
+    }
+    if (!checked) {
+        alert("You must select at least one player type");
+        return false;
+    }
+
+
 
 }
 
